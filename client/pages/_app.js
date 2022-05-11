3106447fee3413cd9ac5d57066ab441e642fbbcf
Nabil <nabil6391@gmail.com>
2022-05-11 18:03:13 +0800

    Fix client for deploying in docker

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,8 +3,8 @@ import { AuthProvider } from "../util/authProvider"
 import { UseContextProvider } from "../context/StepperContext";
 import { UseLanguageProvider } from "../context/LanguageContext";
 
-export const SERVER_URL = process.env.NODE_ENV === 'production' ? "https://produrl.com" : "http://localhost:3080"
-// export const SERVER_URL = process.env.NODE_ENV === 'production' ? "https://produrl.com" : "http://node_backend:3080"
+// export const SERVER_URL = process.env.NODE_ENV === 'production' ? "https://produrl.com" : "http://localhost:3080"
+export const SERVER_URL = process.env.NODE_ENV === 'production' ? "https://produrl.com" : "http://node_backend:3080"
 
 Number.prototype.round = function (places) {
   return +(Math.round(this + "e+" + places) + "e-" + places);
