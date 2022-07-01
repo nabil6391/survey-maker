5aa0ec78cea5a3944e35ae507dcf199cdf9f7ab1
Nabil <nabil6391@gmail.com>
2022-07-01 16:53:33 +0800

    add progress dialog

--- client/pages/[slug]/pdf_review.js
+++ client/pages/[slug]/pdf_review.js
@@ -91,7 +91,8 @@ export default function pdf_review(props) {
   const [mColor, setColor] = useState(Color === 'true' ?? true)
 
 
-  const [dialogOpen, setDialogOpen] = useState(true)
+  const [dialogOpen, setDialogOpen] = useState(print != 'true')
+  const [dialogWait, setDialogWait] = useState(false)
 
   console.log(mDBar, mBar, mDPie)
 
@@ -341,7 +342,7 @@ export default function pdf_review(props) {
                     </label>
                   </div>
 
-                  <a href={`/api/pdf?url=${encodeURIComponent(`${router.asPath}?DBar=${mDBar}&DPie=${mDPie}&mmBar=${mBar}&mmRadar=${mRadar}&Color=${mColor}&print=true`)}`} download="generated_pdf.pdf" className="block text-black" id=''>Generate</a>
+                  <a onClick={(e) => setDialogWait(true)} href={`/api/pdf?url=${encodeURIComponent(`${router.asPath}?DBar=${mDBar}&DPie=${mDPie}&mmBar=${mBar}&mmRadar=${mRadar}&Color=${mColor}&print=true`)}`} download="generated_pdf.pdf" className="block text-black" id=''>Generate</a>
                 </div>
               </div>
             </Transition.Child>
@@ -349,8 +350,52 @@ export default function pdf_review(props) {
         </Dialog >
       </Transition >
 
+      <Transition appear show={dialogWait} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={(value) => setDialogWait(false)}
+        >
+          <div className="min-h-screen px-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="fixed inset-0" />
+            </Transition.Child>
+
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="bg-white inline-block max-h-md w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform shadow-xl rounded-2xl">
+
+                Please wait for 2 mins to generate the pdf
+              </div>
+            </Transition.Child>
+          </div>
+        </Dialog >
+      </Transition >
       <div className="bg-white pl-5 pr-5" width="210mm">
-        {print != 'true' && <button
+        {print !== 'true' && <button
           type="button"
           className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500 print:hidden"
           onClick={() => { setDialogOpen(true) }}
